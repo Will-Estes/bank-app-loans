d71d09707ba55d0b21e397d3fdae24bdec1bb002
Will-Estes <will_estes@yahoo.com>
2023-02-10 11:30:16 -0700

    enable tracing via micrometer

--- src/main/java/com/westes/loans/controller/LoansController.java
+++ src/main/java/com/westes/loans/controller/LoansController.java
@@ -6,21 +6,22 @@ import com.westes.loans.model.Properties;
 import com.westes.loans.repository.LoanRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class LoansController {
 
   private final LoansServiceConfig loansServiceConfig;
   private final LoanRepository loanRepository;
 
   @GetMapping("/loans/{customerId}")
-  public List<Loan> getLoansDetails(@RequestHeader("westes-correlation-id") String correlationId,
-      @PathVariable int customerId) {
+  public List<Loan> getLoansDetails(@PathVariable int customerId) {
+    log.info("getCustomerDetails started");
     return loanRepository.findAllByCustomerIdOrderByStartDtDesc(customerId);
   }
 
